aa3e9393d4e4270bd69ed18a7cd5f2228c87053c
KEVIN01306 <kevin60sanchez31@gmail.com>
2026-04-22 11:06:59 -0600

    commit - nav

--- src/i18n/ui.i18n.ts
+++ src/i18n/ui.i18n.ts
@@ -1,18 +1,10 @@
 export const ui = {
     es: {
-        nav: {
-            home: 'Inicio',
-            about: 'Nosotros',
-        },
         footer: {
             rights: '® Todos los derechos reservados.',
         },
     },
     en: {
-        nav: {
-            home: 'Home',
-            about: 'About Us',
-        },
         footer: {
             rights: '® All rights reserved.',
         },
